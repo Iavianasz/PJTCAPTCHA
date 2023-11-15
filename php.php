912c97396a4f5282fe6ac0e8b8698821c449051e
Iago Viana <iago63803@gmail.com>
2023-11-15 12:41:45 -0300

    first commit

---
+++ php.php
@@ -0,0 +1,30 @@
+<?php
+$recaptcha_secret = '6LfvsA8pAAAAAJwjEPTAdpt4BIA_AiFl3ZOssYwR';
+
+$recaptcha_response = $_POST['g-recaptcha-response'];
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+
+$data = array(
+    'secret' => $recaptcha_secret,
+    'response' => $recaptcha_response
+);
+
+$options = array(
+    'http' => array (
+        'method' => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+$response = json_decode($result, true);
+
+if ($response['success'] == true) {
+    // O reCAPTCHA foi verificado com sucesso
+    // Prossiga com o processamento do formulário
+} else {
+    // O reCAPTCHA falhou
+    // Exiba uma mensagem de erro ou tome outras medidas apropriadas
+}
+?>
