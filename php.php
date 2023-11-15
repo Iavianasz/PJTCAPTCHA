b04ab781fe6e23b35c836c5719d08d2fe719d1fd
Iago Viana <iago63803@gmail.com>
2023-11-15 14:25:01 -0300

    fix

--- php.php
+++ php.php
@@ -29,15 +29,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ];
 
     $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    $result_json = json_decode($result, true);
 
-    // Verifique se o reCAPTCHA foi validado
-    if ($result_json['success']) {
-        // Processar o restante do formulário aqui
-        echo "reCAPTCHA validado. Formulário processado com sucesso!";
+    // Tente fazer a solicitação HTTP
+    $result = @file_get_contents($url, false, $context);
+
+    // Verifique se a solicitação foi bem-sucedida
+    if ($result !== false) {
+        $result_json = json_decode($result, true);
+
+        // Verifique se o reCAPTCHA foi validado
+        if ($result_json['success']) {
+            // Processar o restante do formulário aqui
+            echo "reCAPTCHA validado. Formulário processado com sucesso!";
+        } else {
+            echo "Falha na validação do reCAPTCHA.";
+        }
     } else {
-        echo "Falha na validação do reCAPTCHA.";
+        echo "Erro na solicitação HTTP para o Google reCAPTCHA.";
     }
 } else {
     echo "Erro: O formulário não foi submetido.";
