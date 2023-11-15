959ac25a5c7ec5b9236b65d379627d59b8f3a922
Iago Viana <iago63803@gmail.com>
2023-11-15 16:09:38 -0300

    fix

--- php.php
+++ php.php
@@ -1,53 +1,33 @@
 <?php
-// Verifique se o formulário foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verifique se o nome e o email foram preenchidos
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
+    if($_POST){
+        $curl = curl_init();
 
-    if (empty($nome) || empty($email)) {
-        echo "Erro: Nome e email são campos obrigatórios.";
+        curl_setopt_array($curl,[
+            CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => [
+                'secret' => '6LdC_BApAAAAAMcjXaupF9TBl_49IH_TwQggWLgT'
+                'response' => $_POST['g-recaptcha-response'] ?? ''
+            ]
+        ]);
+
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $responseArray = json_decode($response,true);
+
+        $sucesso = $responseArray['success'] ?? false;
+
+        echo $sucesso ? "Usuário cadastrado com sucesso!" : "RECAPTCHA inválido";
         exit;
+
+
+
+
+
+
+
     }
-
-    // Verifique o reCAPTCHA
-    $recaptcha_secret_key = "6LdC_BApAAAAAMcjXaupF9TBl_49IH_TwQggWLgT";
-    $recaptcha_response = $_POST['g-recaptcha-response'];
-
-    $url = 'https://www.google.com/recaptcha/api/siteverify';
-    $data = [
-        'secret' => $recaptcha_secret_key,
-        'response' => $recaptcha_response
-    ];
-
-    $options = [
-        'http' => [
-            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method' => 'POST',
-            'content' => http_build_query($data)
-        ]
-    ];
-
-    $context = stream_context_create($options);
-
-    // Tente fazer a solicitação HTTP
-    $result = @file_get_contents($url, false, $context);
-
-    // Verifique se a solicitação foi bem-sucedida
-    if ($result !== false) {
-        $result_json = json_decode($result, true);
-
-        // Verifique se o reCAPTCHA foi validado
-        if ($result_json['success']) {
-            // Processar o restante do formulário aqui
-            echo "reCAPTCHA validado. Formulário processado com sucesso!";
-        } else {
-            echo "Falha na validação do reCAPTCHA.";
-        }
-    } else {
-        echo "Erro na solicitação HTTP para o Google reCAPTCHA.";
-    }
-} else {
-    echo "Erro: O formulário não foi submetido.";
-}
 ?>
