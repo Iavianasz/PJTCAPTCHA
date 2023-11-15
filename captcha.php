49b7fcd12a0e1e410cc5f7d50b111578172ead50
Iago Viana <iago63803@gmail.com>
2023-11-15 16:58:25 -0300

    fix

---
+++ captcha.php
@@ -0,0 +1,34 @@
+<?php
+    if($_POST){
+        $curl = curl_init();
+
+        curl_setopt_array($curl,[
+            CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => [
+                'secret' => '6LdC_BApAAAAAMcjXaupF9TBl_49IH_TwQggWLgT',
+                'response' => $_POST['g-recaptcha-response'] ?? ''
+            ]
+        ]);
+
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        
+        $responseArray = json_decode($response,true);
+
+        $sucesso = $responseArray['success'] ?? false;
+
+        echo $sucesso ? "Usuário cadastrado com sucesso!" : "RECAPTCHA inválido";
+        exit;
+
+
+
+
+
+
+
+    }
+?>
