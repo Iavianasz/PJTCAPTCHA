4017e987558dc186c52ab99fbae24ce859632a9a
Iago Viana <iago63803@gmail.com>
2023-11-15 16:14:37 -0300

    fix

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+
+<?php
+    if($_POST){
+        $curl = curl_init();
+
+        curl_setopt_array($curl,[
+            CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => [
+                'secret' => '6LdC_BApAAAAAMcjXaupF9TBl_49IH_TwQggWLgT'
+                'response' => $_POST['g-recaptcha-response'] ?? ''
+            ]
+        ]);
+
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $responseArray = json_decode($response,true);
+
+        $sucesso = $responseArray['success'] ?? false;
+
+        echo $sucesso ? "Usuário cadastrado com sucesso!" : "RECAPTCHA inválido";
+        exit;
+
+
+
+
+
+
+
+    }
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
