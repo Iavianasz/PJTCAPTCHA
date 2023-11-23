94e9fe73b1ddae35e9764c887d1c1a7f12df916e
Pedro <pedrotenerelli257@gmail.com>
2023-11-22 21:38:04 -0300

    fix

--- captcha/captcha.php
+++ captcha/captcha.php
@@ -1,34 +1,53 @@
 <?php
-    if($_POST){
-        $curl = curl_init();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $recaptchaResponse = $_POST['g-recaptcha-response'] ?? '';
 
-        curl_setopt_array($curl,[
-            CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => http_build_query ([
-                'secret' => '6LdC_BApAAAAAMcjXaupF9TBl_49IH_TwQggWLgT',
-                'response' => $_POST['g-recaptcha-response'] ?? ''
-            ])
-        ]);
-
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        
-        $responseArray = json_decode($response,true);
-
-        $sucesso = $responseArray['success'] ?? false;
-
-        echo $sucesso ? "Usuário cadastrado com sucesso!" : "RECAPTCHA inválido";
+    // Verificar se a resposta do reCAPTCHA está presente
+    if (!$recaptchaResponse) {
+        echo "Por favor, complete o reCAPTCHA.";
         exit;
-
-
-
-
-
-
-
     }
+
+    // Chave secreta do reCAPTCHA
+    $secretKey = '6LdC_BApAAAAAMcjXaupF9TBl_49IH_TwQggWLgT';
+
+    // Verificar o reCAPTCHA usando cURL
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => [
+            'secret' => $secretKey,
+            'response' => $recaptchaResponse,
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    // Decodificar a resposta JSON
+    $responseData = json_decode($response, true);
+
+    // Verificar se a validação foi bem-sucedida
+    if ($responseData && $responseData['success'] == true) {
+        // A validação do reCAPTCHA foi bem-sucedida
+        echo "Usuário cadastrado com sucesso!";
+    } else {
+        // A validação do reCAPTCHA falhou
+        echo "Falha na validação do reCAPTCHA.";
+    }
+} else {
+    // Se a solicitação não for POST, redirecione ou lide de acordo com sua lógica
+    echo "Método de requisição inválido.";
+}
 ?>
+
+
+
+
+
+
+
+
